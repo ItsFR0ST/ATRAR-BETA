91a84c66278a60944bf3e455f5ecf04b3f6504de
Frost <54192369+ItsFR0ST@users.noreply.github.com>
2020-11-20 18:51:57 +0200

    Update avatar.js

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -5,7 +5,7 @@ const Discord = require("discord.js")
 module.exports.run = async (client, message, args) => {
     botFetch = client.users.cache.get('732645141839609958')
 
-    let user = (message.guild.member(message.mentions.users.first()) || message.guild.members.cache.get(args[1]) || message.author)   
+    let user = message.mentions.users.first() || client.users.cache.get(args[0])
     let replies = ["Looks Good", "This epic an avatar", "Nice avatar", "This avatar is good", "Nice"];
     let result = Math.floor((Math.random() * replies.length));
     if (!user === message.author) message.channel.send(embed)
